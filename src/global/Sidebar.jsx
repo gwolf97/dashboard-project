9a335379a28476f8bb7ca0d15d6a084e6f1d906b
Gabriel Wolf <92558787+gwolf97@users.noreply.github.com>
2022-11-04 12:47:16 -0500

    Sidebar first draft

--- src/global/Sidebar.jsx
+++ src/global/Sidebar.jsx
@@ -1,8 +1,108 @@
 import React from 'react'
+import { useState } from 'react'
+import {ProSidebar, Menu, MenuItem} from "react-pro-sidebar"
+import "react-pro-sidebar/dist/css/styles.css"
+import {Box, IconButton, Typography, useTheme, Avatar} from "@mui/material"
+import {Link} from "react-router-dom"
+import {tokens} from "../theme"
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined"
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined"
+import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined"
+import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined"
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined"
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined"
+import HelpOutlinedIcon from "@mui/icons-material/HelpOutlined"
+import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined"
+import PieChartOutlinedIcon from "@mui/icons-material/PieChartOutlined"
+import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined"
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined"
+import MapOutlinedIcon from "@mui/icons-material/MapOutlined"
+
 
 const Sidebar = () => {
+    const theme = useTheme()
+    const colors = tokens(theme.palette.mode)
+    const [isCollapsed, setIsCollapsed] = useState(false)
+    const [selected, setSelected] = useState("Dashboard")
+    
   return (
-    <div>Sidebar</div>
+    <Box
+        sx={{
+            "& .pro-sidebar-inner":{
+                background: `${colors.primary[400]} !important`
+            },
+            "& .pro-icon-wrapper": {
+                backgroundColor: "transparent !important"
+            },
+            "& .pro-inner-item":{
+                padding: "5px 35px 5px 20px !important"
+            },
+            "& .pro-inner-item:hover":{
+                color: "#868dfb !important"
+            },
+            "& .pro-menu-item.active":{
+                color: "#6870fa !important"
+            }
+        }}
+    > 
+    <ProSidebar collapsed={isCollapsed}>
+    <Menu iconShape="square">
+      {/* LOGO AND MENU ICON */}
+      <MenuItem
+        onClick={() => setIsCollapsed(!isCollapsed)}
+        icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
+        style={{
+          margin: "10px 0 20px 0",
+          color: colors.grey[100],
+        }}
+      >
+        {!isCollapsed && (
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            ml="15px"
+          >
+            <Typography variant="h3" color={colors.grey[100]}>
+                DASHAPP
+            </Typography>
+            <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
+              <MenuOutlinedIcon />
+            </IconButton>
+          </Box>
+        )}
+      </MenuItem>
+
+      {!isCollapsed && (
+        <Box mb="25px">
+          <Box display="flex" justifyContent="center" alignItems="center">
+            <Avatar
+              alt="profile-user"
+              width="100px"
+              height="100px"
+              src={`../images/profile-pic.jpeg`}
+              style={{ width:"100px", height:"100px"}}
+            />
+          </Box>
+          <Box textAlign="center">
+            <Typography
+              variant="h2"
+              color={colors.grey[100]}
+              fontWeight="bold"
+              sx={{ m: "10px 0 0 0" }}
+            >
+              Gabriel Wolf
+            </Typography>
+            <Typography variant="h5" color={colors.greenAccent[500]}>
+              VP Fancy Admin
+            </Typography>
+          </Box>
+        </Box>
+      )}
+
+    </Menu>
+    </ProSidebar>
+    </Box>
   )
 }
 
