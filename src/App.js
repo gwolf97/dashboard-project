30952174b1f61effcba9c49084a2422240bd0c4b
Gabriel Wolf <92558787+gwolf97@users.noreply.github.com>
2022-11-08 18:56:58 -0500

    Contacts page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Dashboard from "./scenes/dashboard";
 import { Route, Routes } from "react-router-dom";
 import Team from "./scenes/team/index";
 //import Invoices from "./scenes/invoices";
-//import Contacts from "./scenes/contacts";
+import Contacts from "./scenes/contacts";
 //import Form from "./scenes/form";
 //import Line from "./scenes/line";
 //import Bar from "./scenes/bar";
@@ -30,9 +30,9 @@ function App() {
             <Topbar/>
             <Routes>
               <Route path="/" element={<Dashboard/>} />
-               <Route path="/team" element={<Team/>} />
-{/*                <Route path="/contacts" element={<Contacts/>} />
-              <Route path="/invoices" element={<Invoices/>} />
+              <Route path="/team" element={<Team/>} />
+              <Route path="/contacts" element={<Contacts/>} />
+    {/*           <Route path="/invoices" element={<Invoices/>} />
               <Route path="/form" element={<Form/>} />
               <Route path="/bar" element={<Bar/>} />
               <Route path="/pie" element={<Pie/>} />
