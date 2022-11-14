66f5ab25d51500d4deeed89c039254f19580435e
Gabriel Wolf <92558787+gwolf97@users.noreply.github.com>
2022-11-14 15:38:20 -0500

    Fullcalendar

--- src/scenes/calendar/index.jsx
+++ src/scenes/calendar/index.jsx
@@ -14,7 +14,7 @@ const Calender = () => {
     const colors = tokens(theme.palette.mode)
     const [currentEvents, setCurrentEvents] = useState([])
 
-    const handleDataClick = (selected) => {
+    const handleDateClick = (selected) => {
         const title = prompt("Please enter a new title for your event")
         const calendarApi = selected.view.calendar
         calendarApi.unselect()
@@ -64,6 +64,36 @@ const Calender = () => {
                         </ListItem>
                     ) )}
                 </List>
+            </Box>
+
+            {/* Calendar */}
+            <Box flex="1 1 100%" ml="15px">
+                <FullCalendar
+                    height="75vh"
+                    plugins={[
+                        dayGridPlugin,
+                        timeGridPlugin,
+                        interactionPlugin,
+                        listPlugin
+                    ]}
+                    headerToolbar={{
+                        left:"prev,next today",
+                        center:"title",
+                        right:"dayGridMonth,timeGridWeek,timeGridDay,listMonth"
+                    }}
+                    initialView= "dayGridMonth"
+                    editable={true}
+                    selectable={true}
+                    selectMirror={true}
+                    dayMaxEvents={true}
+                    select={handleDateClick}
+                    eventClick={handleEventClick}
+                    eventsSet={(events) => setCurrentEvents(events) }
+                    initialEvents={[
+                        {id:"1234", title:"all-day event", date:"2022-09-14"},
+                        {id:"12345", title:"timed event", date:"2022-09-28"},
+                    ]}
+                />
             </Box>
         </Box>
     </Box>
