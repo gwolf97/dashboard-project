7641395c05d3ace91612e3557653494f7d291347
Gabriel Wolf <92558787+gwolf97@users.noreply.github.com>
2022-11-14 16:25:57 -0500

    Bar chart

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
 import Form from "./scenes/form";
 //import Line from "./scenes/line";
-//import Bar from "./scenes/bar";
+import Bar from "./scenes/bar/index";
 //import Pie from "./scenes/pie";
 import Faq from "./scenes/faq/index";
 //import Geography from "./scenes/geography";
@@ -33,11 +33,11 @@ function App() {
               <Route path="/team" element={<Team/>} />
               <Route path="/contacts" element={<Contacts/>} />
               <Route path="/invoices" element={<Invoices/>} />
-             <Route path="/form" element={<Form/>} />
-            <Route path="/calendar" element={<Calendar/>} /> 
-            <Route path="/faq" element={<Faq/>} />
-     {/*            <Route path="/bar" element={<Bar/>} />
-              <Route path="/pie" element={<Pie/>} />
+              <Route path="/form" element={<Form/>} />
+              <Route path="/calendar" element={<Calendar/>} /> 
+              <Route path="/faq" element={<Faq/>} />
+              <Route path="/bar" element={<Bar/>} />
+          {/*            <Route path="/pie" element={<Pie/>} />
               <Route path="/line" element={<Line/>} />
               <Route path="/geography" element={<Geography/>} />
               */}
